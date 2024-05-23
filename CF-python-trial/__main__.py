5d6ee2b1dac6cc0e17c9d8c274f921457c302fa0
GayathriG1985 <61275765+GayathriG1985@users.noreply.github.com>
2024-05-23 21:48:18 +0530

    Update __main__.py

--- CF-python-trial/__main__.py
+++ CF-python-trial/__main__.py
@@ -1,7 +1,40 @@
+
 from neo4j import GraphDatabase
 
+def get_versions(session, agency):
+    query = "MATCH (a:Agency {agencyID: $agency})-->(v:Version) RETURN v LIMIT 10"
+    result = session.run(query, agency=agency)
+    details = []
+    for record in result:
+         print(record)
+         details.append(record['v']._properties["version"])
+        # details.append({
+        #   "label": record['v']._properties["version"],
+        #   "value": {
+        #     "input": {
+        #       "text": record['v']._properties["version"]
+        #     }
+        #   }
+        # })
+    print(details)
+    return details
 
-def main(params):               
+
+def main():
+
+     neo4j_uri = 'neo4j+s://a2903c3d.databases.neo4j.io'
+     neo4j_user = 'neo4j'
+     neo4j_password = 'x7lO8GKrglcmm4MYuHcBp_PJx23STanbAUKfnuj_FIg'
+     driver = GraphDatabase.driver(neo4j_uri, auth=(neo4j_user, neo4j_password))
+     details = []
+     agency = "X"
+     title = "Please select "
+     with driver.session() as session:
+          if agency:
+               details = get_versions(session, agency)
+               title += "a version"
+               print (details)
+               
      return {
           # specify headers for the HTTP response
           # we only set the Content-Type in this case, to 
@@ -9,8 +42,8 @@ def main(params):
           "headers": {
               "Content-Type": "text/plain;charset=utf-8",
           },
-          
+          "body": details          
           ## use the text generator to create a response sentence
           #  with 10 words
-          "body": "details"
-      }
+     }
+main()
