73efe2025f2b5aa711dd2a2c9591d476f3f068d8
GayathriG1985 <61275765+GayathriG1985@users.noreply.github.com>
2024-05-23 13:43:35 +0530

    Update __main__.py

--- CF-python-trial/__main__.py
+++ CF-python-trial/__main__.py
@@ -1,20 +1,7 @@
 from neo4j import GraphDatabase
 
 
-def main(params):
-
-     neo4j_uri = 'neo4j+s://a2903c3d.databases.neo4j.io'
-     neo4j_user = 'neo4j'
-     neo4j_password = 'x7lO8GKrglcmm4MYuHcBp_PJx23STanbAUKfnuj_FIg'
-     driver = GraphDatabase.driver(neo4j_uri, auth=(neo4j_user, neo4j_password))
-     details = []
-     agency = "X"
-     title = "Please select "
-   #  with driver.session() as session:
-    #      if agency:
-            #   details = get_versions(session, agency)
-               #title += "a version"
-               
+def main(params):               
      return {
           # specify headers for the HTTP response
           # we only set the Content-Type in this case, to 
